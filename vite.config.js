9263f05479ed51563b76931e2779e0e75d886baa
aloksingh06 <aloksingh6761@gmail.com>
2024-12-17 23:06:31 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       '@': '/src', // Ensure this points to your 'src' folder
